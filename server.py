b499d83751e8979b46064d9fd6b96755918a36d6
xbexbex <petja.valkama@gmail.com>
2018-12-22 15:12:50 +0000

    Migrated to quart

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 import requests
-import asyncio
 import os
-from flask import Flask, request
+import json
+import asyncio
+from quart import Quart, request
 from dotenv import load_dotenv
 from telethon import TelegramClient
 
 load_dotenv(override=True)
 BOT_KEY = os.getenv("BOT_KEY")
 SECRET_PATH = os.getenv("SECRET_PATH")
+BOT_ID = os.getenv("BOT_ID")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+PHONE_NUMBER = os.getenv("PHONE_NUMBER")
 
-app = Flask(__name__)
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+app = Quart(__name__)
+loop = asyncio.get_event_loop()
 
 @app.route('/')
 def index():
@@ -24,18 +28,38 @@ def print_bot():
     print(str(SECRET_PATH))
     return(str(SECRET_PATH))
 
+
 @app.route('/' + str(BOT_KEY), methods=['POST'])
-def print_secret():
+def webhook_receive():
     print("segret " + str(request.data))
+    data = json.loads(request.data)
+    print(request.data)
+    if 'new_chat_participant' in data['message']:
+        if str(data['message']['new_chat_participant']['id']) == str(BOT_ID):
+            new_chat_action(data['message']['chat']['id'])
     return("segret " + str(request.headers))
+
+
+@app.route('/chat', methods=['GET'])
+def get_chat_data():
+    id = request.args.get('id')
+    client = TelegramClient('session_name', API_ID, API_HASH)
+    loop.run_until_complete(client.start())
+    return("ddd")
+
+
+def new_chat_action(id: int):
+    print(id)
+
 
 if __name__ == "__main__":
     app.run(
-        ssl_context=('scrub.pem', 'scrub.key'),
+        certfile='scrub.pem', 
+        keyfile='scrub.key',
         host='0.0.0.0',
-        port='8443'
+        port='8443',
+        debug=1
     )
-
 
 """ def get_url(method):
     return "https://api.telegram.org/bot{}/{}".format(BOT_KEY, method)
